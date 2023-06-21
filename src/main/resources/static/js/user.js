5aa625f1e661af6b59f2165b47c233ff08c8c878
k1220 <ky-1220@naver.com>
2023-06-22 03:02:51 +0900

    로그인기능 구현 1차

---
+++ src/main/resources/static/js/user.js
@@ -0,0 +1,57 @@
+let index = {
+	init: function() {
+		$("#btn-save").on("click", () => {
+			this.save();
+		});
+		$("#btn-login").on("click", () => {
+			this.login();
+		});
+		
+	},
+
+	save: function() {
+		//alert("함수호출확인")
+		let data = {
+			username: $("#username").val(),
+			password: $("#password").val(),
+			email: $("#email").val()
+		}
+
+		$.ajax({
+			type: "POST",
+			url: "/blog/api/user",
+			data: JSON.stringify(data),
+			contentType: "application/json;charset=utf-8",
+			dataType: "json"
+		}).done(function(resp) {
+			alert("회원가입완료!");
+
+			location.href = "/blog";
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		});
+	},
+
+	login: function() {
+		//alert("함수호출확인")
+		let data = {
+			username: $("#username").val(),
+			password: $("#password").val()
+		}
+
+		$.ajax({
+			type: "POST",
+			url: "/blog/api/user/login",
+			data: JSON.stringify(data),
+			contentType: "application/json;charset=utf-8",
+			dataType: "json"
+		}).done(function(resp) {
+			alert("로그인완료!!");
+			location.href = "/blog";
+		}).fail(function(error) {
+			alert(JSON.stringify(error));
+		});
+	}
+}
+
+index.init();
---
+++ src/main/webapp/WEB-INF/views/layout/footer.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<br />
+<div class="jumbotron text-center" style="margin-bottom: 0">
+	<p>Created by Stars</p>
+	<p>010-222-2222</p>
+	<p>서울시 관악구 xx동</p>
+</div>
+</body>
+</html>
